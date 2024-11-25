bec1908f74e409fdc79b1681ba241f02158d514d
umm e habiba <161445850+UmmeHabiba1312@users.noreply.github.com>
2024-11-25 13:23:02 +0500

    Update easyandfast.tsx

--- src/components/easyandfast.tsx
+++ src/components/easyandfast.tsx
@@ -1,99 +1,119 @@
-import React from 'react'
-import Image from 'next/image'
-import { ImLeaf } from "react-icons/im";
-import { RiKanbanView2 } from "react-icons/ri";
-import { FaLocationArrow } from "react-icons/fa";
-import { HiChartBar } from "react-icons/hi2";
-import { FaRegHeart } from "react-icons/fa6";
+import React from 'react';
+import Image from 'next/image';
+import { ImLeaf } from 'react-icons/im';
+import { RiKanbanView2 } from 'react-icons/ri';
+import { FaLocationArrow } from 'react-icons/fa';
+import { HiChartBar } from 'react-icons/hi2';
+import { FaRegHeart } from 'react-icons/fa6';
 
 const EasyAndFast = () => {
   return (
-    <main className='flex mt-16 gap-[100px] mx-10'>
-        {/* left */}
-        <div className='w-[50%] pl-10 '>
-        <h3 className='text-gray-600 font-bold'>Easy and Fast</h3>
-        <h1 className='text-[25px] text-gray-800 font-bold mt-1'>
-            Book Your Next Trip In 3 Easy Steps
+    <main className="flex flex-col lg:flex-row items-center mt-16 px-4 lg:px-16 gap-16">
+      {/* Left Section */}
+      <div className="lg:w-1/2 w-full lg:pl-16 mb-8 lg:mb-0">
+        <h3 className="text-gray-600 font-semibold uppercase">Easy and Fast</h3>
+        <h1 className="text-3xl sm:text-4xl text-gray-800 font-bold mt-1">
+          Book Your Next Trip In 3 Easy Steps
         </h1>
-        <div className="one flex mt-8 gap-3 ">
-        <Image
-             className='h-14 w-14'
-            src={'/one.PNG'}
-            alt='img'
-            height={500}
-            width={500}
+
+        <div className="mt-8 space-y-8">
+          {/* Step 1 */}
+          <div className="flex items-center gap-4">
+            <Image
+              className="h-14 w-14"
+              src="/one.PNG"
+              alt="Step 1: Choose Destination"
+              height={500}
+              width={500}
             />
-           <div className=''>
-
-           <h4 className='text-gray-600  font-bold '>Choose Destination</h4>
-           <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur inventore culpa voluptas provident, corporis, ipsum vel dolorem repudiandae eaque.</p>
-           </div>
-        </div>
-        <div className="two">
-        <div className="one flex mt-8 gap-3">
-        <Image
-             className='h-14 w-14'
-            src={'/two.PNG'}
-            alt='img'
-            height={500}
-            width={500}
-            />
-           <div className=''>
-
-           <h4 className='text-gray-600  font-bold'>Make Payment</h4>
-           <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur inventore culpa voluptas provident, corporis, ipsum vel dolorem repudiandae eaque.</p>
-           </div>
-        </div>
-        </div>
-        <div className="three">
-        <div className="one flex mt-8 gap-3">
-        <Image
-             className='h-14 w-14'
-            src={'/three.PNG'}
-            alt='img'
-            height={500}
-            width={500}
-            />
-           <div className=''>
-
-           <h4 className='text-gray-600  font-bold'>Reach Airpot on Selected Date</h4>
-           <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur inventore culpa voluptas provident, corporis, ipsum vel dolorem repudiandae eaque.</p>
-           </div>
-        </div>
-        </div>
-        </div>
-
-        {/* right */}
-        <div className='w-[35%]'>
-            <div className='rounded-2xl  hover:bg-slate-50 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300'>
-                <div className="parent flex justify-center rounded-2xl m-7 ">
-                <Image
-                className='rounded-2xl h-48 w-[400px] mt-8'
-               src='/Greece.webp'
-               alt='img'
-               height={500}
-               width={500}/>
-                </div>
-                <div className='mt-8 ml-8'>
-                <h3 className='text-2xl font-bold text-gray-600 flex'>Trip To Greece</h3>
-                <p className='text-gray-500 mt-4'>11-24 JUNE| by Umm e habiba</p>
-                <div className='flex gap-8 mt-5 '>
-                <p className='bg-slate-100 p-2 rounded-full'><ImLeaf /></p>
-                <p className='bg-slate-100 p-2 rounded-full'><RiKanbanView2 /></p>
-                <p className='bg-slate-100 p-2 rounded-full'><FaLocationArrow /></p>
-                </div>
-                <div className='flex  mt-6 gap-[240px]'>
-                  <div className="1 flex ">               
-                     <p className='bg-slate-100 p-2 rounded-full'><HiChartBar /></p>
-                  <p>24 People going</p></div>
-                  <div className="2">              
-                      <p className='bg-slate-100 p-2 rounded-full'><FaRegHeart /></p></div>
-                </div>
-                </div>
+            <div>
+              <h4 className="text-gray-600 font-semibold">Choose Destination</h4>
+              <p className="text-gray-500">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur inventore culpa voluptas provident, corporis, ipsum vel dolorem repudiandae eaque.
+              </p>
             </div>
-        </div>
-    </main>
-  )
-}
+          </div>
 
-export default EasyAndFast
+          {/* Step 2 */}
+          <div className="flex items-center gap-4">
+            <Image
+              className="h-14 w-14"
+              src="/two.PNG"
+              alt="Step 2: Make Payment"
+              height={500}
+              width={500}
+            />
+            <div>
+              <h4 className="text-gray-600 font-semibold">Make Payment</h4>
+              <p className="text-gray-500">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur inventore culpa voluptas provident, corporis, ipsum vel dolorem repudiandae eaque.
+              </p>
+            </div>
+          </div>
+
+          {/* Step 3 */}
+          <div className="flex items-center gap-4">
+            <Image
+              className="h-14 w-14"
+              src="/three.PNG"
+              alt="Step 3: Reach Airport"
+              height={500}
+              width={500}
+            />
+            <div>
+              <h4 className="text-gray-600 font-semibold">Reach Airport on Selected Date</h4>
+              <p className="text-gray-500">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur inventore culpa voluptas provident, corporis, ipsum vel dolorem repudiandae eaque.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Right Section */}
+      <div className="lg:w-1/3 w-full">
+        <div className="rounded-2xl shadow-lg hover:shadow-2xl transition-transform transform hover:scale-105 duration-300 overflow-hidden">
+          <div className="relative">
+            <Image
+              className="rounded-2xl w-full h-64 object-cover"
+              src="/Greece.webp"
+              alt="Trip to Greece"
+              height={500}
+              width={500}
+            />
+          </div>
+          <div className="p-6">
+            <h3 className="text-2xl font-bold text-gray-600">Trip to Greece</h3>
+            <p className="text-gray-500 mt-2">11-24 June | by Umm e Habiba</p>
+
+            <div className="flex gap-4 mt-6">
+              <p className="bg-gray-100 p-3 rounded-full">
+                <ImLeaf className="text-green-500" />
+              </p>
+              <p className="bg-gray-100 p-3 rounded-full">
+                <RiKanbanView2 className="text-indigo-500" />
+              </p>
+              <p className="bg-gray-100 p-3 rounded-full">
+                <FaLocationArrow className="text-blue-500" />
+              </p>
+            </div>
+
+            <div className="flex items-center justify-between mt-6">
+              <div className="flex items-center gap-2">
+                <p className="bg-gray-100 p-3 rounded-full">
+                  <HiChartBar className="text-orange-500" />
+                </p>
+                <p className="text-gray-500">24 People going</p>
+              </div>
+              <p className="bg-gray-100 p-3 rounded-full">
+                <FaRegHeart className="text-red-500" />
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default EasyAndFast;
