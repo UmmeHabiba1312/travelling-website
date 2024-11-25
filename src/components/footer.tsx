d7fd105fa87fc1a2d11d9f59ddeca02b2da45c57
umm e habiba <161445850+UmmeHabiba1312@users.noreply.github.com>
2024-11-25 13:21:35 +0500

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
   return (
-    <main className=' h-[100px] mt-40 '>
-      <hr />
-<div className=' '>       
-     <p className='text-gray-600 font-bold text-[16px] flex justify-center
-        '>Made with love by Umm e Habiba</p></div>
-       <div> <p className='flex justify-center text-gray-600 font-semibold'>All rights reserved @Jadoo.com</p></div>
+    <main className="h-[100px] mt-40">
+      <hr className="border-gray-300" />
+      <div className="flex flex-col items-center justify-center mt-4 space-y-2">
+        {/* Made with love text */}
+        <p className="text-gray-600 font-bold text-sm sm:text-base md:text-lg">
+          Made with love by Umm e Habiba
+        </p>
+        {/* All rights reserved text */}
+        <p className="text-gray-600 font-semibold text-xs sm:text-sm md:text-base">
+          All rights reserved @Jadoo.com
+        </p>
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
