e086ae36e894f91a92079a76f9517d702dc1e85b
umm e habiba <161445850+UmmeHabiba1312@users.noreply.github.com>
2024-11-25 13:23:55 +0500

    Update logo.tsx

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,44 +1,56 @@
-import React from 'react'
-import Image from 'next/image'
+import React from 'react';
+import Image from 'next/image';
 
 const Logo = () => {
   return (
-    <main className=' mx-40 my-16'>
-        <div className='flex'>
-            <div className="1 flex m-[45px]">
-                <Image
-                className='size-fit'
-                height={500}
-                width={500}
-                src='/logo1.png'
-                alt='logoimg'/>
-            </div>
-            <div className="2  flex m-[45px]">
-            <Image
-                className='pb-[10px] size-fit'
-                height={500}
-                width={500}
-                src='/logo2.png'
-                alt='logoimg'/>
-            </div>
-            <div className="3  flex m-[45px]">
-            <Image
-                className='size-fit'
-                height={500}
-                width={500}
-                src='/logo3.png'
-                alt='logoimg'/>
-            </div>
-            <div className="4 flex m-[50px]  pt-10">               
-                 <Image
-                className='size-fit'
-                height={500}
-                width={500}
-                src='/logo4.png'
-                alt='logoimg'/></div>
+    <main className="mx-4 lg:mx-40 my-16">
+      <div className="flex flex-wrap justify-center md:justify-between items-center gap-8">
+        {/* Logo 1 */}
+        <div className="flex justify-center">
+          <Image
+            className="object-contain"
+            src="/logo1.png"
+            alt="Logo 1"
+            height={200} // Adjust height as needed
+            width={200}  // Adjust width as needed
+          />
         </div>
-    </main>
-  )
-}
+        
+        {/* Logo 2 */}
+        <div className="flex justify-center">
+          <Image
+            className="object-contain pb-2"
+            src="/logo2.png"
+            alt="Logo 2"
+            height={200} // Adjust height as needed
+            width={200}  // Adjust width as needed
+          />
+        </div>
 
-export default Logo
+        {/* Logo 3 */}
+        <div className="flex justify-center">
+          <Image
+            className="object-contain"
+            src="/logo3.png"
+            alt="Logo 3"
+            height={200} // Adjust height as needed
+            width={200}  // Adjust width as needed
+          />
+        </div>
+
+        {/* Logo 4 */}
+        <div className="flex justify-center pt-10">
+          <Image
+            className="object-contain"
+            src="/logo4.png"
+            alt="Logo 4"
+            height={200} // Adjust height as needed
+            width={200}  // Adjust width as needed
+          />
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Logo;
